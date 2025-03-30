09f33e63e6b1e7cdd6b110126ed09c93537f6aeb
AntonioVelez24 <fabricio.velez@tecsup.edu.pe>
2025-03-30 18:28:35 -0500

    Menu Scene

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    [SerializeField] GameObject amongus;
+    [SerializeField] float rotationSpeed;
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+    void Update()
+    {
+        amongus.transform.Rotate(0,0, rotationSpeed * Time.deltaTime);
+
+    }
+}
